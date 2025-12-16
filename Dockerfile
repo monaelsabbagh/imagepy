65f116a4d49ba6bea23c49ed95af92ff91521e1c
monaelsabbagh <monaelsabbagh02@gmail.com>
2025-12-16 18:57:47 +0200

    image py

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,5 @@ FROM python:3.10
 WORKDIR /app
 
 COPY file.py .
-COPY requirements.txt .
 
-RUN pip install --no-cache-dir -r requirements.txt
-
-EXPOSE 5000
-
-CMD ["python", "file.py"]
+CMD sh -c "python file.py; cd /; sh"
--- file.py
+++ file.py
@@ -1,12 +1,8 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+print("================================")
+print("   Hello from Python Docker 🐍  ")
+print("================================")
 
-class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(b"Hello from Python Docker App")
+name = input("Enter your name: ")
+print(f"Welcome, {name}!")
 
-server = HTTPServer(("0.0.0.0", 5000), MyHandler)
-print("Server running on port 5000...")
-server.serve_forever()
+print("\nProgram finished successfully.")
